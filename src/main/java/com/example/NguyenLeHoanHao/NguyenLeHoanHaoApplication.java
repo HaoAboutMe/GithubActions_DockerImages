ddbe418fc0c0cf5d8f04caaa7fbacc156d950b31
Nguyen Le Hoan Hao <nguyenlehoanhao2004@gmail.com>
2026-03-20 15:50:08 +0700

    feat: Init Project

---
+++ src/main/java/com/example/NguyenLeHoanHao/NguyenLeHoanHaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenLeHoanHao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenLeHoanHaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenLeHoanHaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NguyenLeHoanHao/NguyenLeHoanHaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenLeHoanHao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenLeHoanHaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
